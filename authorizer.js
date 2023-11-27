1993ed24c393c5eb631d9f63c5198c022f670f59
daniele <zeolla.d@gmail.com>
2023-11-27 19:42:33 +0100

    Fixed somme issue with the resource

--- authorizer.js
+++ authorizer.js
@@ -8,6 +8,34 @@ module.exports.handler = function(event, context, callback) {
     const path = event.path;
     
     console.log(`host ${host}, path: ${path}, user-agent: ${userAgent}`)
+    console.log(event)
 
-    callback(null, { statusCode: 200, body: "OK" });
+    if (headers['policy'] == 'deny') {
+        callback(null, generatePolicy('Deny', event.methodArn));
+    } else {
+        callback(null, generatePolicy('Allow', event.methodArn));
+    }
+}
+
+/**
+ * Generates a policy with the specified effect.
+ *
+ * @param {string} effect - The effect of the policy (Allow or Deny)
+ * @return {Object} The generated policy object.
+ */
+function generatePolicy(effect, resource) {
+
+    return {
+        principalId: "user",
+        policyDocument: {
+            Version: "2012-10-17",
+            Statement: [
+                {
+                    Action: "execute-api:Invoke",
+                    Effect: effect,
+                    Resource: resource
+                }
+            ]
+        }
+    }
 }

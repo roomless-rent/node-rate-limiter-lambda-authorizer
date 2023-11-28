2ccd42dd41ace0bb9573db5b446315b9cdff1abe
daniele <zeolla.d@gmail.com>
2023-11-28 13:36:09 +0100

    Added token validation and rate limiter for admin user and non admin user

--- authorizer.js
+++ authorizer.js
@@ -1,37 +1,80 @@
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 const { RateLimiterDynamo } = require("rate-limiter-flexible");
+const jwt = require('jsonwebtoken');
+
+const RATE_LIMITER_SECONDS = 60;
+const dynamoClient = new DynamoDB();
+
+const rateLimiter = new RateLimiterDynamo({
+    storeClient: dynamoClient,
+    points: Number(process.env.DEFAULT_RATE_LIMIT) | 30,
+    duration: RATE_LIMITER_SECONDS,
+    tableName: 'api-rate-limiter-prod',
+    tableCreated: true,
+    keyPrefix: 'not-admin'
+});
+
+const rateLimiterAdmin = new RateLimiterDynamo({
+    storeClient: dynamoClient,
+    points: Number(process.env.ADMIN_RATE_LIMIT) | 90,
+    duration: RATE_LIMITER_SECONDS,
+    tableName: 'api-rate-limiter-prod',
+    tableCreated: true,
+    keyPrefix: 'admin'
+});
 
 module.exports.handler = async function(event, context, callback) {
-
-    const dynamoClient = new DynamoDB();
-    const rateLimiter = new RateLimiterDynamo({
-        storeClient: dynamoClient,
-        points: 10,
-        duration: 60,
-        tableName: 'api-rate-limiter-prod',
-        tableCreated: true
-    });
 
     const request = {
         headers: event.headers,
         context: event.requestContext,
         path: event.requestContext.path,
         sourceIp: event.requestContext.identity.sourceIp,
-        userAgent: event.requestContext.identity.userAgent
+        userAgent: event.requestContext.identity.userAgent,
     }
 
     if (!request.sourceIp) {
         callback(null, generatePolicy('Deny', event.methodArn, {}));
     }
 
-    try {
-        const res = await rateLimiter.consume(request.sourceIp);
-        console.log(res);
-        callback(null, generatePolicy('Allow', event.methodArn, res));
-    } catch(e) {
-        console.log(e);
-        callback(null, generatePolicy('Deny', event.methodArn, e));
+    let token = null;
+
+    if (request.headers['Authorization']) {
+        const bearer = request.headers['Authorization'].replace('Bearer ', '');
+        console.log(`Bearer: ${bearer}`);
+        console.log(process.env)
+        try {
+            token = jwt.verify(bearer, process.env.JWT_SECRET);
+            console.log(`Verified token: ${JSON.stringify(token)}`);
+        } catch(e){
+            console.error(`Failed to verify token ${e}`);
+        }
     }
+
+    let rateLimiterRes = null;
+    let policy = null;
+
+    // Use different rate limiter for admins
+    if (token && token.roles?.includes(1)) { //Todo fix this hardcode, 1 is admin
+        console.log(`Using admin rate limiter for ip: ${request.sourceIp}, email: ${token.email}`);
+        try {
+            rateLimiterRes = await rateLimiterAdmin.consume(request.sourceIp);
+            policy = generatePolicy('Allow', event.methodArn);
+        } catch(e) {
+            policy = generatePolicy('Deny', event.methodArn);
+        }
+    } else {
+        console.log(`Using default rate limiter for ip ${request.sourceIp}, email: ${token?.email || 'N/A'}`);
+        try {
+            rateLimiterRes =  await rateLimiter.consume(request.sourceIp);
+            policy = generatePolicy('Allow', event.methodArn);
+        } catch(e) {
+            policy = generatePolicy('Deny', event.methodArn);
+        }
+    }
+
+    console.log(`Rate limiter result for ip ${request.sourceIp}(${request.userAgent}): ${rateLimiterRes}`);
+    callback(null, policy);
 }
 
 /**
